71adaeb829fc9841015a60a4ea768c98cc911a77
Caio Eduardo <souza.caioeduardo@gmail.com>
2021-07-29 16:53:16 -0300

    test: created tests suites and PageObjects for `Calculator` components

---
+++ src/__tests__/PageObjects/DisplayPageObject.js
@@ -0,0 +1,24 @@
+import { fireEvent } from '@testing-library/vue';
+import Utils from '../utils';
+import PageObjectBase from './PageObjectBase';
+
+export default class DisplayPageObject extends PageObjectBase {
+  constructor(container) {
+    super();
+    
+    this.inputDisplay = container.queryByTestId('input-display');
+  }
+
+  async type(expression) {
+    await fireEvent.update(this.inputDisplay, expression);
+  }
+
+  async typeRandomExpressionOnInputDisplay() {
+    const expression = Utils.getRandomExpression();
+    await this.type(expression);
+  }
+
+  async pressEnterOnInputDisplay() {
+    await this.pressEnterOn(this.inputDisplay);
+  }
+}
